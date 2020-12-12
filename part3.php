8feefb59ed4ed1dbe3d71101397fea528451a6b9
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-12-12 17:22:19 +0900

    [Add] Lecture1-1.2

--- part3.php
+++ part3.php
@@ -2,11 +2,19 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<link rel="stylesheet" href="part3.css">
 	<title>part3</title>
 </head>
 <body>
+	<h1>Lecture1-1.1</h1>
 	<?php 
 		print('PHPを勉強中です！');
+	?>
+	<h1>Lecture1-1.2</h1>
+	<?php 
+		print('<h2 class="lecture1-1-2-title">画面にHTMLを表示する</h2>');
+		print('<p>PHPはHTMLの中に埋め込んで記述することができます。</p>');
+		print('<p>改行するときは、<br>brタグを記述します。');
 	?>
 </body>
 </html>

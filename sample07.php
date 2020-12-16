7f6647135186f65e40f695237ed6d124d9eb7dc0
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-12-16 10:21:39 +0900

    [Add] Lecture1-2.1練習問題

--- sample07.php
+++ sample07.php
@@ -7,8 +7,16 @@
 <body>
 	<select name="age" id="age">
 		<?php 
-			for ($i=10; $i <= 70; $i++) { 
-				print('<option value="' . $i . '">' . $i . '歳</option>');
+			for ($i=10; $i <= 70 ; $i++) { 
+				print('<option value="'.$i.'">'.$i.'歳</option>');
+			}
+		?>
+	</select>
+
+	<select name="even_age" id="even_age">
+		<?php
+			for ($i=10; $i <= 70 ; $i=$i+2) { 
+				print('<option value="'.$i.'">'.$i.'歳</option>');
 			}
 		?>
 	</select>

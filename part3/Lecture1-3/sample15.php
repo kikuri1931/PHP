fe40c8a7e4d99f4b6fe66f3a844ad1fa5401b4b9
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-12-20 12:22:24 +0900

    [Add] Lecture1-3

---
+++ part3/Lecture1-3/sample15.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Lecture1-3.1</title>
+</head>
+<body>
+	<?php 
+		header('Location: http://h2o-space.com/');
+		exit();
+	?>
+</body>
+</html>

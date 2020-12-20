1e84974c74ed2b6e9f2ef92217bd68ae28312e5a
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-12-20 18:04:22 +0900

    [Add] Lecture1-3.8

---
+++ part3/Lecture1-3/sample22.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Lecture1-3.8</title>
+</head>
+<body>
+	<?php 
+		$date = sprintf('%04d年 %02d月 %02d日', 2010, 1, 23);
+	?>
+	<?php 
+		print($date);
+	?>
+</body>
+</html>

ea095aac3bd1d215ca057275eca8d77a259e9534
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-12-28 13:26:18 +0900

    [Add] step10

--- shop/index.php
+++ shop/index.php
@@ -3,12 +3,28 @@
 <head>
 	<meta charset="utf-8">
 	<title>practice1</title>
+	<link rel="stylesheet" href="practice1.css">
 </head>
 <body>
 	<?php 
 		require('dbconnect.php');
-		$recordSet = mysqli_query($db, 'SELECT m.name, i.* FROM makers m, my_items i WHERE m.id=i.maker_id ORDER BY id DESC')
+		$sql = 'SELECT COUNT(*) AS cnt FROM my_items';
+		$recordSet = mysqli_query($db, $sql);
+		$table = mysqli_fetch_assoc($recordSet);
+		$maxPage = ceil($table['cnt']/5);
+		if (
+		  	isset($_GET["page"]) &&
+		 	$_GET["page"] > 0 &&
+		  	$_GET["page"] <= $maxPage
+		) {
+		  	$page = (int)$_GET["page"];
+		} else {
+		  	$page = 1;
+		}
+		$start = ($page - 1) * 5;
+		$recordSet = mysqli_query($db, 'SELECT m.name, i.* FROM makers m, my_items i WHERE m.id=i.maker_id ORDER BY id DESC LIMIT '.$start.', 5');
 	?>
+	<h1>｜ 商品管理</h1>
 	<table width="100%">
 		<tr>
 			<th scope="col">ID</th>
@@ -37,5 +53,22 @@
 			} 
 		?>
 	</table>
+		<ul class="paging">
+			<?php if ($page > 1) { ?>
+				<li class="paging-list">	
+					<a href="index.php?page=<?php print($page - 1); ?>">前のページへ</a>
+				</li>
+			<?php } else { ?>
+				<li class="paging-list">前のページへ</li>
+			<?php } ?>
+
+			<?php if($page < $maxPage) { ?>
+				<li class="paging-list">
+					<a href="index.php?page=<?php print($page + 1); ?>">次のページへ</a>
+				</li>
+			<?php } else { ?>
+				<li class="paging-list">次のページへ</li>
+			<?php } ?>	
+		</ul>
 </body>
 </html>

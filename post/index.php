67179d5bd9947e0f3c0e89429ae1c6d846bc69ea
kikuri1931 <ys1945050mite.blue@gmail.com>
2021-01-09 15:49:21 +0900

    [Add] practice2 step7

---
+++ post/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>ひとこと掲示板</title>
+	<link rel="stylesheet" href="../join/practice2.css">
+</head>
+<body>
+	<div id="wrap">
+		<div id="head">
+			<h1>| ひとこと掲示板</h1>
+		</div>
+		<div id="content">
+			<form method="post">
+				<h4>メッセージをどうぞ</h4>
+				<textarea name="message" cols="50" rows="5"></textarea>
+				<div>
+					<input type="submit" value="送信する">
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
